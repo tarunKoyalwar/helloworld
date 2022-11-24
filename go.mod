5f050e687fe229d93adcffbe2d0d4bffcfee7e56
Tarun Koyalwar <tarun@projectdiscovery.io>
2022-11-24 19:34:52 +0530

    v0.1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tarunKoyalwar/helloworld
+
+go 1.19
--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World v0")
+	fmt.Println("Hello World v0.1")
 }
